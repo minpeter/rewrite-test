83e9885b4d8ee3e380bd880525bd6d3e77b252d1
minpeter <kali2005611@gmail.com>
2025-08-11 20:34:08 +0900

    Implement page layout and routing configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/:path*",
+          destination: `https://friendli.ai/:path*`,
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
